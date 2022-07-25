a9b73616781a0e1a89b8672315a49fc4efc90b65
Yuni1906 <109393356+Yuni1906@users.noreply.github.com>
2022-07-25 11:42:20 +0800

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -9,9 +9,9 @@ handler.before = async function (m, { isAdmin, isBotAdmin }) {
   if (chat.antiLink && isGroupLink && !isAdmin && !m.isBaileys && m.isGroup) {
     let thisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
     if (m.text.includes(thisGroup)) throw false // jika link grup itu sendiri gak dikick
-      if (!isBotAdmin) m.reply(` *「 ANTILINK 」* ${isAdmin ? "Admin mah bebas ygy :'v" : `\n\nlink group terdeteksi dan ${global.namabot} bukan admin jadi tidak bisa ngekick!`}`)
+      if (!isBotAdmin) m.reply(` *「 ANTILINK 」* ${isAdmin ? "𝖮𝗁 𝖠𝖽𝗆𝗂𝗇 𝗋𝗎𝗉𝖺𝗇𝗒𝖺 :'v" : `\n\nlink group terdeteksi dan ${global.namabot} bukan admin jadi tidak bisa ngekick!`}`)
     if (isBotAdmin) {
-      m.reply(` *「 ANTILINK 」* \n\nLink Group Terdeteksi, bye Kamu Akan Di Kick!!`.trim())
+      m.reply(` *「 𝘼𝙉𝙏𝙄𝙇𝙄𝙉𝙆 𝙒𝙆𝙒𝙆 」* \n\n𝘓𝘪𝘯𝘬 𝘨𝘳𝘶𝘱 𝘵𝘳𝘥𝘦𝘵𝘦𝘬𝘴𝘪,𝘮𝘢𝘢𝘧,𝘜𝘯𝘪 𝘢𝘬𝘢𝘯 𝘮𝘦𝘯𝘨𝘦𝘭𝘶𝘢𝘳𝘬𝘢𝘯𝘮𝘶!!`.trim())
       await this.delay(500)
       await this.groupParticipantsUpdate(m.chat, [m.sender], "remove")
     }
